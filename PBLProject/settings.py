a8da877a0408335d35f2353edbf6c807a008fc74
alex <aclefos@yahoo.com>
2020-10-30 13:54:19 +0200

    installed django-heroku

--- PBLProject/settings.py
+++ PBLProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,5 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y']
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+
+django_heroku.settings(locals())
